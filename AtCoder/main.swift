54420d68bda34a6b91415d820041be07f273c67f
rec0516 <akira.coffee@gmail.com>
2023-04-28 13:44:14 +0900

    ソース管理しない設定ファイルを追加

--- AtCoder/main.swift
+++ AtCoder/main.swift
@@ -1,11 +1,22 @@
-//
-//  main.swift
-//  AtCoder
-//
-//  Created by 佐藤亮 on 2023/04/28.
-//
-
 import Foundation
 
-print("Hello, World!")
+func main()  {
 
+    let n = readInt()
+
+}
+main()
+
+
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
+
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
+}
