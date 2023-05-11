2f865900da891fe19f96fe490fe7601a765d6f7a
rec0516 <akira.coffee@gmail.com>
2023-05-11 11:09:15 +0900

    abc224のa問題を回答

---
+++ abc200/abc220/abc224/a/main.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func main()  {
+
+    let s = readLine()!
+
+    if s.suffix(2) == "er" {
+        print("er")
+        return 
+    }
+
+    print("ist")
+
+}
+main();
+
+
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
+
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
+}
+
