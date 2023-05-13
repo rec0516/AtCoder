a7df014a0dcc871a334dc872ebf551385973ec7d
rec0516 <akira.coffee@gmail.com>
2023-05-13 18:37:55 +0900

    abc226のa問題を回答

---
+++ abc200/abc220/abc226/a/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func main()  {
+
+    let x = readLine()!
+
+    let val: Double = Double(x)!
+    print(Int(round(val)))
+}
+main();
+
+
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
+
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
+}
+
